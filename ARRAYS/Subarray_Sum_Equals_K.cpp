065e4f734a3e2d7b9b991d4697afd30c41dd250b
Your Name <uditi>
2026-06-01 17:19:05 +0530

    Number of Subarrays with xor K

--- ARRAYS/Subarray_Sum_Equals_K.cpp
+++ ARRAYS/Subarray_Sum_Equals_K.cpp
@@ -1,39 +1,46 @@
-//better solution 
-// #include <iostream>
-// using namespace std;
 
-// int main() {
-//     int k;
-//     int count=0;
-//     int arr[]={1,2,3,4,5,3,1,2,3,4};
-//      int n = sizeof(arr) / sizeof(arr[0]);
-//     cout<<"enter the value ok 'K'";
-//     cin>>k;
-//     for (int i = 0; i < n; i++)
-//     {
-//         int sum=0;
-//         for (int j = i; j < n; j++)
-//         {
-//            sum+=arr[j];
-//            if (sum==k)
-//         {
-//             count++;
-//         }
-        
-//         }
-        
-//     }
-    
-//       cout << "Number of subarrays with sum " << k << " = " << count;
-//     return 0;
-// }
-
-// Brute force 
+//------> Brute force
+// O(N3)
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    int k;
+    int count = 0;
+    int arr[] = {1, 2, 3, 4, 5, 3, 1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "enter the value ok 'K'";
+    cin >> k;
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = i; j < n; j++)
+        {
+            int sum = 0;
+            for (int l = i; l < j; l++)
+            {
+                sum += arr[l];
+            }
+            if (sum == k)
+            {
+                count++;
+            }
+        }
+    }
+    cout << "Number of subarrays with sum " << k << " = " << count;
+
+    return 0;
+}
+
+
+//---->better solution
+// O(N2)
+ #include <iostream>
+ using namespace std;
+
 int main() {
-      int k;
+    int k;
     int count=0;
     int arr[]={1,2,3,4,5,3,1,2,3,4};
      int n = sizeof(arr) / sizeof(arr[0]);
@@ -41,23 +48,21 @@ int main() {
     cin>>k;
     for (int i = 0; i < n; i++)
     {
-        
+        int sum=0;
         for (int j = i; j < n; j++)
         {
-            int sum=0;
-            for (int l = i; l < j; l++)
-            {
-                sum+=arr[l];
-           
-            }
-              if (sum==k)
-                {
-                    count++;
-                }
+           sum+=arr[j];
+           if (sum==k)
+        {
+            count++;
         }
-        
+
+        }
+
     }
-    cout << "Number of subarrays with sum " << k << " = " << count;
-    
+
+      cout << "Number of subarrays with sum " << k << " = " << count;
     return 0;
 }
+
+// optimal solution
---
+++ ARRAYS/no_of_Subarrays_with_xor_k.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+ using namespace std;
+
+int main() {
+    int k;
+    int count=0;
+    int arr[]={4,2,2,6,4};
+     int n = sizeof(arr) / sizeof(arr[0]);
+    cout<<"enter the value ok 'K'";
+    cin>>k;
+    for (int i = 0; i < n; i++)
+    {
+        int xr=0;
+        for (int j = i; j < n; j++)
+        {
+           xr=xr^arr[j];
+           if (xr==k)
+        {
+            count++;
+        }
+
+        }
+
+    }
+
+      cout << "Number of Subarrays with xor K  " << k << " = " << count;
+    return 0;
+}
