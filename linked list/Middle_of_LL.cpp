f12392f8fbe40c3585ed46dd18495c4325f8cb5d
Udit Bhardwaj <uditbhardwaj655@gmail.com>
2026-06-25 21:56:04 +0530

    find middle of linked list with tortoise and hare

---
+++ linked list/Middle_of_LL.cpp
@@ -0,0 +1,20 @@
+
+
+// tortoise and hare 
+
+
+
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != NULL && fast->next != NULL) {
+
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
